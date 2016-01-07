445e42d316b4562a34be446cf4b8de42217f8148
Ali Baghernejad <AliBaghernezhad@gmail.com>
2016-01-07 15:24:32 +0330

    Add RouletteWheelSelection file

---
+++ TSP-AntColony/RouletteWheelSelection.fs
@@ -0,0 +1,11 @@
+﻿namespace AntColonyOptimization
+
+open AntColonyOptimization.RequiredTypes
+open AntColonyOptimization.Settings
+
+module Selectors = 
+    let RouletteWheelSelection p =
+        let r = rnd.NextDouble()
+        let c = p|> Array.mapi (fun idx elem -> p.[..idx] |> Array.sum) 
+        let cc= c |> Array.tryFindIndex (fun elem -> r <= elem) 
+        cc
