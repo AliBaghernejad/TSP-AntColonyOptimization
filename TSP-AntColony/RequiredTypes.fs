b3d94bff88cc77d696a55d8efe2c1d91c351224c
Ali Baghernejad <AliBaghernezhad@gmail.com>
2016-01-07 15:22:16 +0330

    Add RequiredTypes module

---
+++ TSP-AntColony/RequiredTypes.fs
@@ -0,0 +1,11 @@
+﻿namespace AntColonyOptimization
+
+ // Type Definition
+module RequiredTypes= 
+
+    let rnd = System.Random()    // Random generator func
+    type Route = int []
+    [<Measure>] type km          // kilometre unit of measure
+    type Ant = {mutable Route : Route; mutable Cost : float }
+
+
